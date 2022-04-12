6fbadc8605d1bfde67deccfbe613f946894e6893
guricerin <chanbo1e9@gmail.com>
2022-04-12 12:49:34 +0900

    p.59

---
+++ src/ch3/3_2_1_cas-atomic.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+// アトミックなCAS
+bool compare_and_swap(uint64_t *p, uint64_t val, uint64_t newval)
+{
+    return __sync_bool_compare_and_swap(p, val, newval);
+}
