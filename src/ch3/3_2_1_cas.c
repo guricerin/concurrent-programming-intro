b80c0db64b469c1c4dd84e4bb1c740e860a16add
guricerin <chanbo1e9@gmail.com>
2022-04-12 12:09:40 +0900

    p.58

---
+++ src/ch3/3_2_1_cas.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+// アトミックではないCAS
+bool compare_and_swap(uint64_t *p, uint64_t val, uint64_t newval)
+{
+    if (*p != val)
+    {
+        return false;
+    }
+    *p = newval;
+    return true;
+}
